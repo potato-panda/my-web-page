30bae1ba81a7a565a3d8c1259586f410e5dcbb0e
potato-panda <76853151+potato-panda@users.noreply.github.com>
2024-05-05 15:38:55 -0400

    Some changes
    Updated font to be more mobile responsive
    Added link class
    Updated font colour
    Changed some padding

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1 class="name">Zhu Jian Kenny Su</h1>
     <div class="card">
-      <a href="mailto:zhujiankennysu@gmail.com?subject=Inquiry&body=Hello%20Kenny,">zhujiankennysu@gmail.com</a>
-      <a href="tel:+1(647)785-0457">(647) 785-457</a>
+      <a class="link" href="mailto:zhujiankennysu@gmail.com?subject=Inquiry&body=Hello%20Kenny,">zhujiankennysu&#8203;@gmail.com</a>
+      <a class="link" href="tel:+1(647)785-0457">(647) 785-457</a>
     </div>
   </div>
 `
