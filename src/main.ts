7f6e63c37ff933e888b666b5ac87599dd4b6178d
potato-panda <76853151+potato-panda@users.noreply.github.com>
2024-05-05 13:47:02 -0400

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1 class="name">Zhu Jian Kenny Su</h1>
+    <div class="card">
+      <a href="mailto:zhujiankennysu@gmail.com?subject=Inquiry&body=Hello%20Kenny,">zhujiankennysu@gmail.com</a>
+      <a href="tel:+1(647)785-0457">(647) 785-457</a>
+    </div>
+  </div>
+`
